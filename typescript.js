0ea13cfcb05a731277d48a32f6ac1faa4f70ea2f
Fansy <418536538@qq.com>
2023-01-09 19:34:45 +0800

    feat: 新增 TypeScript 配置项

---
+++ typescript.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['plugin:@typescript-eslint/recommended'],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  rules: {}
+};
